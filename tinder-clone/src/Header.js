67fcbd8734430ad16616a8353e4412bb03633649
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-23 05:02:11 -0700

    Added 'person-icon' from material-ui and made it a button with IconButton

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./Header.css";
-import PersonIcon from "@material-ui/icons/Person"
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
     return (
         <div className="header">
             <p1>I am the header</p1>
-            <PersonIcon />
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon" />
+            </IconButton>
+            
         </div>
     )
 }

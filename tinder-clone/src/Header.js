bb91fe691564db09161b3a7a23012b3ad1848402
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-23 05:13:39 -0700

    Added code to Header.css to align the header icons and space them appart

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -6,11 +6,16 @@ import IconButton from '@material-ui/core/IconButton';
 function Header() {
     return (
         <div className="header">
-            <p1>I am the header</p1>
             <IconButton>
                 <PersonIcon fontSize="large" className="header__icon" />
             </IconButton>
             
+            <img
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo-700x394.png"
+                alt=""
+            />
+
         </div>
     )
 }

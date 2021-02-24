fb5ddfb0468fbc8616c25bec5ebc62de9af3336e
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-23 21:41:45 -0700

    Added two people' s information to the TinderCards.js file

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name: "Elon Musk",
+            url: "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
+        },
+        {
+            name: "Jeff Bezos",
+            url: "https://www.biography.com/.image/c_fill%2Ccs_srgb%2Cfl_progressive%2Ch_400%2Cq_auto:good%2Cw_620/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
+        },
+    ]);
+
     return (
         <div className="tinderCards">
-            
+            <div className="tinderCards__cardContainer">
+                {people.map((person) => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={["up", "down"]}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}>
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     )
 }
